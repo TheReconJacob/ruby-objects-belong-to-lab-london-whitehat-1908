4e57a1ccaca77d5a3743e422c22ee6e491aea99c
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-13 07:54:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
  
-  attr_accessor :song
+  attr_accessor :title, :artist
  
-  def initialize(song)
-    @Song = song
+  def initialize(title)
+    @title = title
     hotline_bling = Song.new("Hotline Bling")
   end
 end
